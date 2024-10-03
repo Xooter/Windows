02347184a32f451abf11247d7f69e6a0ee0901ea
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-03 19:40:20 -0300

    tailwind colors + layout changes

--- Windows/app/_layout.tsx
+++ Windows/app/_layout.tsx
@@ -1,9 +1,29 @@
 import { Stack } from "expo-router";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+  const insets = useSafeAreaInsets();
+
+  var screenOptions = {
+    contentStyle: {
+      backgroundColor: "#fff",
+      paddingTop: insets.top,
+      paddingBottom: insets.bottom,
+      paddingLeft: insets.left,
+      paddingRight: insets.right,
+    },
+  };
+
   return (
-    <Stack>
-      <Stack.Screen name="index" />
+    <Stack
+      initialRouteName="index"
+      screenOptions={{
+        headerShown: false,
+        gestureEnabled: true,
+        animation: "ios",
+      }}
+    >
+      <Stack.Screen name="index" options={screenOptions} />
     </Stack>
   );
 }
--- Windows/tailwind.config.js
+++ Windows/tailwind.config.js
@@ -6,6 +6,17 @@ module.exports = {
     "./app/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      accent: "#EB455F",
+      secondary: "#8696FE",
+      light: "#F1F1F1",
+      error: "#DA4646",
+      success: "#67aa64",
+      dark: {
+        DEFAULT: "#121212",
+        100: "#191919",
+      },
+    },
     extend: {},
   },
   plugins: [],
