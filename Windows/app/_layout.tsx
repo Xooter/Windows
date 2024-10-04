08c22e2ce594ca9cf12bd94f46192fe24d2ca9a4
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-03 21:15:06 -0300

    Added fonts

--- Windows/app/_layout.tsx
+++ Windows/app/_layout.tsx
@@ -1,8 +1,17 @@
 import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { View } from "react-native";
 
 export default function RootLayout() {
   const insets = useSafeAreaInsets();
+
+  const [fontsLoaded] = useFonts({
+    Suse: require("../assets/fonts/suse/SUSE-Regular.ttf"),
+    SuseBold: require("../assets/fonts/suse/SUSE-Bold.ttf"),
+  });
+
+  if (!fontsLoaded) return <View />;
 
   var screenOptions = {
     contentStyle: {
