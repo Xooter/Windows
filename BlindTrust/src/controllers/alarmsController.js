cc2b5a6e06fba3ce9616493fb399eded3a626ac8
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-07 13:58:52 -0300

    controllers crud

--- BlindTrust/src/controllers/alarmsController.js
+++ BlindTrust/src/controllers/alarmsController.js
@@ -1,5 +1,31 @@
-export function getAlarms() {}
+import { db } from "../database.js";
 
-export function createAlarm() {}
+export async function getAlarms(_req, res) {
+  res.send(db.data.alarms);
+}
 
-export function deleteAlarm() {}
+export async function createAlarm(req, res) {
+  const newAlarm = req.body;
+
+  db.data.alarms.push(newRule);
+  db.write();
+  res.send(newAlarm);
+}
+
+export async function deleteAlarm(req, res) {
+  const { id } = req.params;
+
+  const alarmExists = db.data.alarms.find((a) => a.id == id);
+
+  if (!alarmExists) {
+    return res.sendStatus(404);
+  }
+
+  const index = db.data.alarms.indexOf(alarmExists);
+  if (index > -1) {
+    db.data.alarms.splice(index, 1);
+    db.write();
+  }
+
+  res.send(id);
+}
--- BlindTrust/src/controllers/rulesController.js
+++ BlindTrust/src/controllers/rulesController.js
@@ -1,5 +1,31 @@
-export function getRules() {}
+import { db } from "../database.js";
 
-export function createRule() {}
+export async function getRules(_req, res) {
+  res.send(db.data.rules);
+}
 
-export function deleteRule() {}
+export async function createRule(req, res) {
+  const newRule = req.body;
+
+  db.data.rules.push(newRule);
+  db.write();
+  res.send(newRule);
+}
+
+export async function deleteRule(req, res) {
+  const { id } = req.params;
+
+  const alarmExists = db.data.rules.find((a) => a.id == id);
+
+  if (!alarmExists) {
+    return res.sendStatus(404);
+  }
+
+  const index = db.data.rules.indexOf(alarmExists);
+  if (index > -1) {
+    db.data.rules.splice(index, 1);
+    db.write();
+  }
+
+  res.send(id);
+}
