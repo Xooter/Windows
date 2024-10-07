16a8c2e020eff7a20d5ff73eb439d80300570224
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-07 19:57:59 -0300

    load alarms

--- Windows/app/alarms.tsx
+++ Windows/app/alarms.tsx
@@ -1,14 +1,72 @@
 import { ConditionContainer } from "@/components/ConditionContainer";
 import { TitleAdd } from "@/components/TitleAdd";
-import { View } from "react-native";
+import axios from "axios";
+import { useCallback, useEffect, useState } from "react";
+import { ActivityIndicator, Text, View } from "react-native";
+import { BASE_BACK } from "../secrets";
+import { Alarm } from "@/models/Alarm";
+import { Styles } from "@/utils/Styles";
+import { useFocusEffect } from "@react-navigation/native";
 
 export default function Alarms() {
+  const [alarms, setAlarms] = useState<Alarm[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useFocusEffect(
+    useCallback(() => {
+      const getAllAlarms = async () => {
+        setLoading(true);
+        await axios
+          .get(`${BASE_BACK}/alarms`)
+          .then((response) => {
+            setAlarms(response.data);
+            console.log(response.data);
+          })
+          .finally(() => setLoading(false));
+      };
+
+      getAllAlarms();
+    }, []),
+  );
+
+  const formatTime = (timeString: Date): string => {
+    const date = new Date(timeString);
+    return date.toLocaleTimeString([], {
+      hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+    });
+  };
+
   return (
     <View className="flex-col w-full h-full items-center pt-14">
       <TitleAdd title="Alarm" />
-      <ConditionContainer />
-      <ConditionContainer />
-      <ConditionContainer />
+      {!loading ? (
+        alarms.map((alarm) => (
+          <ConditionContainer
+            key={alarm.id}
+            blind={alarm.blind}
+            curtain={alarm.curtain}
+            days={alarm.days}
+            one_time={alarm.one_time}
+            active={alarm.active}
+            onActive={() => {
+              //implementar activar alarma
+            }}
+          >
+            <Text
+              style={{ ...Styles.title, fontSize: 35, color: "#7881ff" }}
+              className="capitalize"
+            >
+              {formatTime(alarm.time)}
+            </Text>
+          </ConditionContainer>
+        ))
+      ) : (
+        <View className="flex items-center justify-center w-full h-full">
+          <ActivityIndicator size="large" color="#222" />
+        </View>
+      )}
     </View>
   );
 }
--- Windows/components/ConditionContainer.tsx
+++ Windows/components/ConditionContainer.tsx
@@ -1,10 +1,31 @@
 import { Styles } from "@/utils/Styles";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome, MaterialCommunityIcons } from "@expo/vector-icons";
 import React from "react";
 import { Text, View } from "react-native";
 import AnimatedSwitch from "./UI/Switch";
 
-export const ConditionContainer = () => {
+export const ConditionContainer = ({
+  children,
+  curtain,
+  blind,
+  days,
+  one_time,
+  active,
+  onActive,
+}: {
+  children: React.ReactNode;
+
+  curtain: number;
+  blind: number;
+
+  days?: number[];
+  one_time?: boolean;
+
+  active: boolean;
+  onActive: (active: boolean) => void;
+}) => {
+  const days_week = ["L", "M", "Mi", "J", "V", "S", "D"];
+
   return (
     <View
       style={[
@@ -15,22 +36,39 @@ export const ConditionContainer = () => {
       ]}
       className="w-[95%] p-5 rounded-xl flex-col mb-4"
     >
-      <Text style={{ ...Styles.title, fontSize: 18 }}>One Time</Text>
-      <View className="flex-row justify-between items-center gap-y-2">
-        <Text style={{ ...Styles.title, fontSize: 35, color: "#7881ff" }}>
-          14:00
+      <View className="flex-row justify-between items-center">
+        <Text
+          style={{
+            ...Styles.title,
+            fontSize: 18,
+            opacity: one_time ? 0.2 : 1,
+          }}
+        >
+          One Time
         </Text>
-        <AnimatedSwitch
-          isOn={true}
-          onToggle={(newState: any) => console.log("Switch state:", newState)}
-        />
+
+        <View className="flex-row items-center gap-x-1">
+          {days_week.map((_: string, index: number) => (
+            <FontAwesome
+              key={index}
+              name="circle"
+              size={9}
+              color={days?.includes(index) ? "#7881ff" : "#dee8ff"}
+            />
+          ))}
+        </View>
+      </View>
+      <View className="flex-row justify-between items-center">
+        {children}
+        <AnimatedSwitch isOn={active} onToggle={onActive} />
       </View>
 
-      <View className="bg-accent h-1 w-full rounded-xl my-1" />
       <View className="flex-row gap-x-2 items-center justify-between">
         <View className="flex-row items-center gap-x-1">
           <MaterialCommunityIcons name="blinds" size={20} color="#87A2FF" />
-          <Text style={{ ...Styles.subtitle, fontSize: 25 }}>100%</Text>
+          <Text style={{ ...Styles.subtitle, fontSize: 25 }}>
+            {blind * 100}%
+          </Text>
         </View>
         <View className="flex-row items-center gap-x-1">
           <MaterialCommunityIcons
@@ -38,7 +76,9 @@ export const ConditionContainer = () => {
             size={20}
             color="#87A2FF"
           />
-          <Text style={{ ...Styles.subtitle, fontSize: 25 }}>25%</Text>
+          <Text style={{ ...Styles.subtitle, fontSize: 25 }}>
+            {curtain * 100}%
+          </Text>
         </View>
       </View>
     </View>
