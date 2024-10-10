12f37a23e5d801f5ae1b28b36592d55c4d41d2a3
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-10 11:10:07 -0300

    weather loading indicator

--- Windows/components/WeatherNow.tsx
+++ Windows/components/WeatherNow.tsx
@@ -1,6 +1,6 @@
 import { Styles } from "@/utils/Styles";
 import { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ActivityIndicator } from "react-native";
 import { WEATHER_URL } from "../secrets";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { weatherConditions } from "@/utils/WeatherConditions";
@@ -10,6 +10,7 @@ import { Weather } from "@/models/Weather";
 
 export const WeatherNow = () => {
   const [weather, setWeather] = useState<Weather | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const getCurrentWeather = async () => {
@@ -20,7 +21,8 @@ export const WeatherNow = () => {
         })
         .catch((error) => {
           console.error(error);
-        });
+        })
+        .finally(() => setLoading(false));
     };
 
     getCurrentWeather();
@@ -31,46 +33,54 @@ export const WeatherNow = () => {
       className="flex-row bg-light items-center justify-between rounded-xl w-[90%] py-5 px-8 mt-20 mb-8"
       style={{ ...Styles.shadow, backgroundColor: "#f8faff" }}
     >
-      <View>
-        <Text
-          style={{ ...Styles.paragraph, fontSize: 20, opacity: 0.8 }}
-          className="capitalize"
-        >
-          {weather?.weather[0].main}
-        </Text>
-        <View className="flex-row items-end">
-          <Text style={[styles.temperature]}>
-            {weather?.main.temp.toFixed(0)}ᵒ
-          </Text>
-          <Text style={styles.temperatureMin}>/</Text>
-          <Text style={styles.temperatureMin}>
-            {weather?.main.temp_min.toFixed(0)}ᵒ
-          </Text>
-        </View>
+      {!loading ? (
+        <>
+          <View>
+            <Text
+              style={{ ...Styles.paragraph, fontSize: 20, opacity: 0.8 }}
+              className="capitalize"
+            >
+              {weather?.weather[0].main}
+            </Text>
+            <View className="flex-row items-end">
+              <Text style={[styles.temperature]}>
+                {weather?.main.temp.toFixed(0)}ᵒ
+              </Text>
+              <Text style={styles.temperatureMin}>/</Text>
+              <Text style={styles.temperatureMin}>
+                {weather?.main.temp_min.toFixed(0)}ᵒ
+              </Text>
+            </View>
 
-        <View className="flex-row items-center gap-x-2">
-          <FontAwesome6 name="wind" size={14} color="black" />
-          <Text style={{ ...Styles.paragraph, fontSize: 20 }}>
-            <Text style={Styles.title}>{weather?.wind.speed}</Text> km/h
-          </Text>
-        </View>
-      </View>
+            <View className="flex-row items-center gap-x-2">
+              <FontAwesome6 name="wind" size={14} color="black" />
+              <Text style={{ ...Styles.paragraph, fontSize: 20 }}>
+                <Text style={Styles.title}>{weather?.wind.speed}</Text> km/h
+              </Text>
+            </View>
+          </View>
 
-      <MaterialCommunityIcons
-        size={70}
-        name={
-          weatherConditions[
-            (weather?.weather[0].main as keyof typeof weatherConditions) ||
-              "Snow"
-          ].icon as any
-        }
-        color={
-          weatherConditions[
-            (weather?.weather[0].main as keyof typeof weatherConditions) ||
-              "Snow"
-          ].color
-        }
-      />
+          <MaterialCommunityIcons
+            size={70}
+            name={
+              weatherConditions[
+                (weather?.weather[0].main as keyof typeof weatherConditions) ||
+                  "Snow"
+              ].icon as any
+            }
+            color={
+              weatherConditions[
+                (weather?.weather[0].main as keyof typeof weatherConditions) ||
+                  "Snow"
+              ].color
+            }
+          />
+        </>
+      ) : (
+        <View className="flex items-center justify-center w-full">
+          <ActivityIndicator size="large" color="#222" />
+        </View>
+      )}
     </View>
   );
 };
