bc8910b923697991924aaea0e948c25748cc0b3a
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-10 11:27:55 -0300

    current curtain/blind info

--- Windows/app/index.tsx
+++ Windows/app/index.tsx
@@ -3,27 +3,58 @@ import CustomSlider from "@/components/UI/Slider";
 import { WeatherNow } from "@/components/WeatherNow";
 import { View } from "react-native";
 import { TitleSlider } from "@/components/TitleSlider";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { BASE_BACK } from "@/secrets";
 
 export default function Index() {
+  type CurrentValues = {
+    curtain: number;
+    blind: number;
+  };
+
+  const [blindValue, setBlindValue] = useState<number>(0);
+  const [curtainValue, setCurtainValue] = useState<number>(0);
+
+  const [currentValues, setCurrentValues] = useState<CurrentValues>({
+    curtain: 0,
+    blind: 0,
+  });
+
+  useEffect(() => {
+    const getInfo = async () => {
+      await axios.get(`${BASE_BACK}/`).then((response) => {
+        setCurrentValues(response.data);
+        console.log(response.data);
+      });
+    };
+
+    getInfo();
+  }, []);
+
   return (
     <View className="flex-col w-full h-full items-center gap-y-5">
       <WeatherNow />
-      <TitleSlider text="Curtain" porcentage={25} />
+      <TitleSlider text="Curtain" porcentage={curtainValue} />
       <CustomSlider
         minValue={0}
         maxValue={100}
-        currentValue={100}
-        progressValue={25}
-        onValueChange={(value: number) => {}}
+        currentValue={0}
+        progressValue={currentValues.curtain}
+        onValueChange={(value: number) => {
+          setCurtainValue(value);
+        }}
         steps={5}
       />
-      <TitleSlider text="Blind" porcentage={50} />
+      <TitleSlider text="Blind" porcentage={blindValue} />
       <CustomSlider
         minValue={0}
         maxValue={100}
-        currentValue={25}
-        progressValue={50}
-        onValueChange={(value: number) => {}}
+        currentValue={0}
+        progressValue={currentValues.blind}
+        onValueChange={(value: number) => {
+          setBlindValue(value);
+        }}
         steps={5}
       />
       <SendButton text="Send" />
