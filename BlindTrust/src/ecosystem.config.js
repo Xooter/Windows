a8da1a9fd7547267cb13803afea074c51555d6cc
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-13 14:12:49 -0300

    fix

--- BlindTrust/src/ecosystem.config.js
+++ BlindTrust/src/ecosystem.config.js
@@ -1,15 +1,13 @@
-module.exports = {
-  apps: [
-    {
-      name: "persiana",
-      script: "index.js",
-      env: {
-        NODE_ENV: "development",
-      },
-      env_production: {
-        NODE_ENV: "production",
-        PORT: 4000,
-      },
+export const apps = [
+  {
+    name: "persiana",
+    script: "index.js",
+    env: {
+      NODE_ENV: "development",
     },
-  ],
-};
+    env_production: {
+      NODE_ENV: "production",
+      PORT: 4000,
+    },
+  },
+];
