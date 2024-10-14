477f3529c1e13b3fb99f4f624efff9ebbd220d84
Joaquin Righetti <joaquinrighetti@gmail.com>
2024-10-14 11:02:08 -0300

    cors

--- BlindTrust/src/index.js
+++ BlindTrust/src/index.js
@@ -14,6 +14,7 @@ import { setCurtain } from "./controllers/hardwareController.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/alarms", alarmsRoutes);
